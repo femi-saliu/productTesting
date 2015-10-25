45c76882a576fd38af331e9a0a86e61c2119b728
Femi Saliu <femisaliu.gmail.com>
2015-10-24 23:23:40 -0700

    [progress] Adds more DB interaction

--- app.js
+++ app.js
@@ -1,27 +1,38 @@
 angular.module('productTesting',["firebase"])
 
-.controller('productTable',['$scope',function($scope){
+.controller('productTable',['$scope', '$firebaseObject', function($scope, $firebaseObject){
   var ref = new Firebase("https://producttesting.firebaseio.com");
-  $scope.data = $firebaseObject(ref);
+  var firebaseObj = $firebaseObject(ref);
+  firebaseObj.$loaded().then(function() {
+    $scope.data = firebaseObj;
+   });
 
   $scope.initDB = function(){
-    $scope.data = { // Clears the database and sets up the tables
+    $scope.data['productTestDB'] = { // Clears the database and sets up the tables
       products: {},
       tests: [],
       product_tests: [],
     };
+    console.log('DB initialized');
+    // add three tests
+    $scope.addTest('Complete Documentation', 'Do the docs meet all of our requirements?');
+    $scope.addTest('QA Test', 'Checks run by Quality Assurance Team');
+    $scope.addTest('Beta Test', 'Initial testing with select beta testers');
+
+    $scope.saveDB();
   };
 
   $scope.saveDB = function(cb){
-    ref.$save().then(function(ref) {
+    firebaseObj.$save().then(function(ref) {
+      ref.key() === firebaseObj.$id; // true
       if (cb) { cb(ref); }
-      else { console.log("Save successful.")}
+      else { console.log("Save successful.", firebaseObj)}
     }, function(error) {
       console.log("Error:", error);
     });
   }
   $scope.addTest = function(name, description){
-    $scope.data.tests.push({
+    $scope.data['productTestDB'].tests.push({
       name: name,
       description: description,
     });
@@ -29,16 +40,55 @@ angular.module('productTesting',["firebase"])
   };
 
   $scope.addProduct = function(name, manufacturer){
-    $scope.data.products[name] = {
+    $scope.data['productTestDB'].products[name] = {
       manufacturer: manufacturer,
       state: "unverified",
     };
+    // Add 3 product tests
+    angular.forEach($scope.data['productTestDB'].tests, function(value, key) {
+       $scope.addProductTest(name, value.name);
+    });
     $scope.saveDB();
   }
 
   $scope.verifyProduct = function(name){
-    $scope.data.products[name].state = "verified";
+    $scope.data['productTestDB'].products[name].state = "verified";
+    $scope.saveDB();
+  };
+
+  $scope.addProductTest = function(product, test){
+    $scope.data['productTestDB'].product_tests[product+'|'+test] = {
+      product: product,
+      test: test,
+      date_time: Date.now(),
+      status: 'pending',
+      comment: '',
+    };
+    $scope.saveDB();
+  };
+
+  $scope.passProductTest = function(product, test){
+    $scope.data['productTestDB'].product_tests[product+'|'+test].status = 'pass';
+    $scope.saveDB();
+  };
+
+  $scope.failProductTest = function(product, test){
+    $scope.data['productTestDB'].product_tests[product+'|'+test].status = 'fail';
+    $scope.saveDB();
+  };
+
+  $scope.setProductTestComment = function(product,test, comment){
+    $scope.data['productTestDB'].product_tests[product+'|'+test].comment = comment;
     $scope.saveDB();
   };
 
 }]);
+// .factory('productDB', ['$scope', '$firebaseObject', function($scope, $firebaseObject){
+
+//   function initDB(){}
+//   function saveDB(){}
+//   function addTest(){}
+//   function addProduct(){}
+//   function verifyProduct(){}
+//   return {};
+// }]);
